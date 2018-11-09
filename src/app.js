af005bf88a811e67eca161740650e03c53be7c49
Nathan Savas <nathansavas@gmail.com>
2018-11-08 22:21:45 -0800

    initialized map interface
    used mapbox to initialize the map interface with long beach as the center

---
+++ src/app.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3N1bGJhY20td2FzdGVwcm9qZWN0IiwiYSI6ImNqbmI3bXIwdDAxMnMzcG8weDRzM3Z1NnkifQ.3V4lCPK2mrT54kGI22fdRQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-118.152117, 33.807431],
+    zoom: 10
+});
