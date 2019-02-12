31d49be1cbe3b8870c182745c84a95c25c92b482
Nathan Savas <nathansavas@gmail.com>
2019-02-12 12:41:53 -0800

    imported d3 and cities csv file, also added encode url query function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
+// Display map
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3N1bGJhY20td2FzdGVwcm9qZWN0IiwiYSI6ImNqbmI3bXIwdDAxMnMzcG8weDRzM3Z1NnkifQ.3V4lCPK2mrT54kGI22fdRQ';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v9',
     zoom: 1
 });
+
+// Fetch us cities csv data
+d3.csv("../data/uscitiesv1.4.csv").then(function(data) {
+    console.log(data);
+});
+
+// Query parameters to be passed to API endpoint
+let params = {};
+  
+// This function takes in a parameter object and
+// encodes it into a URL query
+function encodeQuery(params) {
+    let esc = encodeURIComponent;
+    let query = Object.keys(params)
+    .map(k => esc(k) + '=' + esc(params[k]))
+    .join('&');
+
+    return query;
+}
