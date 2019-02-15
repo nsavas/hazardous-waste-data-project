2c6b0f1b0076e5c5a50df829dfa340c9a8213eaf
Nathan Savas <nathansavas@gmail.com>
2019-02-14 19:48:32 -0800

    +live search for us cities
    Live search functionality has been added to index.html

--- src/app.js
+++ src/app.js
@@ -2,25 +2,54 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3N1bGJhY20td2FzdGVwcm9qZWN0IiwiYSI6ImNqbmI3bXIwdDAxMnMzcG8weDRzM3Z1NnkifQ.3V4lCPK2mrT54kGI22fdRQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v9',
-    zoom: 1
+    style: 'mapbox://styles/mapbox/light-v9',
+    zoom: 4.05,
+    center: [-97.2795, 38.0282]
 });
+
+let geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+})
+
+map.addControl(geocoder);
 
 // Fetch us cities csv data
-d3.csv("../data/uscitiesv1.4.csv").then(function(data) {
-
-    console.log(data);
+/*d3.csv("../data/uscitiesv1.4.csv").then(function(data) {
+    let temp = [];
+    for (let i = 0; i < data.length; i++) {
+        temp.push(data[i].city);
+    }
+    getUSCities()
 });
 
+$.get("../data/uscitiesv1.4.csv", function(data) {
+    console.log(data);
+    temp = data.split("\n");
+    let USCities = []
+    for (let i = 1; i < temp.length; i++) {
+        USCities.push(temp[i].split(",")[0]);
+    }
+    getUSCities(USCities);
+})
+*/
+
+
 // Query parameters to be passed to API endpoint
-let params = {};
+let params = {
+    state: "CA"
+};
 
 // This function takes in a parameter object and
 // encodes it into a URL query
 function encodeQuery(params) {
     let esc = encodeURIComponent;
     let query = Object.keys(params)
-    .map(k => esc(k) + '/' + esc(params[k]))
+    .map(key => esc(key) + '/' + esc(params[key]))
     .join('/');
     return query;
 }
+
+let userQuery = encodeQuery(params);
+
+/*fetch("https://iaspub.epa.gov/enviro/efservice/V_TRI_RELEASES_EZ/" + userQuery, function(data) {
+}).then()*/
