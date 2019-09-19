ce8fda43ff79b75b0f0690b38e956f1795eadef7
Savas, Nathan <nathansavas@gmail.com>
2019-09-18 20:14:34 -0700

    site info goes away once user inputs
    Site info goes away upon user input
    hide access_token as an env variable

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,40 +3,48 @@ import MapboxGl from 'mapbox-gl/dist/mapbox-gl.js';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js';
 import '../App.css';
 
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+
 class Map extends React.Component {
-    mapRef = React.createRef();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showHeader: true
+        }
+    }
 
     componentDidMount() {
-        const ACCESS_TOKEN = 'pk.eyJ1IjoiY3N1bGJhY20td2FzdGVwcm9qZWN0IiwiYSI6ImNqbmI3bXIwdDAxMnMzcG8weDRzM3Z1NnkifQ.3V4lCPK2mrT54kGI22fdRQ';
-        
         MapboxGl.accessToken = ACCESS_TOKEN;
 
-        var map = new MapboxGl.Map({
+        let map = new MapboxGl.Map({
           container: this.container,
           style: 'mapbox://styles/mapbox/light-v9',
           center: [-97.2795, 38.0282],
           zoom: 4.20
         })
 
-        var geocoder = new MapboxGeocoder({
+        let geocoder = new MapboxGeocoder({
             accessToken: ACCESS_TOKEN,
             countries: "US",
             placeholder: "Enter any US city"
         });
 
         document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+        
+        geocoder.on('result', result => {
+            this.setState({ showHeader: false });
 
-        geocoder.on('result', function(result) {
-            console.log(result);
-            var city = result.result.text;
-            console.log(city);
-        })
+            let userCity = result.result.text;
+            console.log(userCity);
+        });
     }
 
     render() {
         return (
             <div className="parentMap">
                 <div className="Map" ref={(x) => { this.container = x }}></div>
+                {this.state.showHeader ? 
                 <div className="header">
                     <div className="header-bg">
                         <h1>US Chemical Release Visualizer</h1>
@@ -46,8 +54,7 @@ class Map extends React.Component {
                             TRI tracks the management of certain toxic chemicals that may pose a threat to human health and the environment.
                             More information may be found on the website.
                         </h2>
-                        <div id="geocoder" className="geocoder">
-                        </div>
+                        <div id="geocoder" className="geocoder"></div>
                         </div>
                         <div className="header-description">
                             <h2>
@@ -60,7 +67,7 @@ class Map extends React.Component {
                             </h2>
                         </div>
                     </div>
-                </div>
+                </div> : null}
             </div>
         );
     }
