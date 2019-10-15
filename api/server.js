616a6c608bfab1bb6fc23e1ac06fde11df49e54e
Savas, Nathan <nathansavas@gmail.com>
2019-10-15 01:49:00 -0700

    update readme

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-// Allows us to request from client to backend
+// Enable CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
