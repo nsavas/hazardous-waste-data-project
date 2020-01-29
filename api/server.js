a833288899ea37296b143d4f1667bf700186c17b
Savas, Nathan <nathansavas@gmail.com>
2020-01-28 23:04:17 -0800

    update query and point layer
    since the result geometry query returns repeated coordinates, I edited the query to group by facilitygeometry, removing repeats. Also changed the circle layer to a symbol layer to add an image as a point

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ const database = process.env.DB_NAME;
 // Initialization options
 let options = {
     promiseLib: promise
-  };
+};
 
 // Set up connection
 let pgp = require('pg-promise')(options); // PostgreSQL and PostGIS module
@@ -34,36 +34,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 // Receives city/state and sends back corresponding data as well as geojson for the heatmap
-app.post('/postgre-api/get-tri-releases-by-city', function(request, response) {
+app.post('/postgre-api/get-tri-releases-by-city', function (request, response) {
     let cityName = request.body.city;
     let cityState = request.body.state;
 
     // Geojson query
     let query1 = "SELECT row_to_json(fc) as result_geometry FROM ("
-                +  "SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ("
-                +  "SELECT 'Feature' As type, ST_AsGeoJSON(facilitygeometry)::json As geometry FROM "
-                +  "full_epa_tri_releases WHERE city = '" + cityName + "' AND state = '" + cityState + "') As f) As fc;"
+        + "SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ("
+        + "SELECT 'Feature' As type, ST_AsGeoJSON(facilitygeometry)::json As geometry FROM "
+        + "full_epa_tri_releases WHERE city = '" + cityName + "' AND state = '" + cityState + "' GROUP BY facilitygeometry) As f) As fc;"
 
     // Data query
     let query2 = "SELECT row_to_json(fc) as result_data FROM (SELECT array_to_json(array_agg(f)) As features FROM("
-                + "SELECT year, facilityname, streetaddress, city, county, state, zipcode, latitude, longitude,"
-                +  "federalfacility, industrysector, chemical, cleanairactchemical, metal, carcinogen, unitofmeasure,"
-                +  "fugitiveair_51, stackair_52, water_53, underground_54, underground_class_i_541, underground_class_ii_v_542,"
-                +  "landfills_551, rcra_c_landfills_551a, otherlandfills_551b, landtreatment_552, surfaceimpoundment_553,"
-                +  "rcra_surfaceimpoundment_553a, othersurfaceimpoundment_553b, otherdisposal_554, onsitereleasetotal,"
-                +  "potw_transfersforrelease_61, potw_transfersfortreatment_61, potw_totaltransfers,"
-                +  "offsitereleasetotal, offsiterecycledtotal, offsiteenergyrecoverytotal, offsitetreatedtotal,"
-                +  "totaltransfer, totalreleases, releases_81, energyrecoveryonsite_82, recyclingonsite_84, treatmentonsite_86,"
-                +  "productionwaste_81_to_87, parentcompanyname FROM full_epa_tri_releases "
-                +  "WHERE city = '" + cityName + "' AND state = '" + cityState + "') As f) As fc";
-    
+        + "SELECT year, facilityname, streetaddress, city, county, state, zipcode, latitude, longitude,"
+        + "federalfacility, industrysector, chemical, cleanairactchemical, metal, carcinogen, unitofmeasure,"
+        + "fugitiveair_51, stackair_52, water_53, underground_54, underground_class_i_541, underground_class_ii_v_542,"
+        + "landfills_551, rcra_c_landfills_551a, otherlandfills_551b, landtreatment_552, surfaceimpoundment_553,"
+        + "rcra_surfaceimpoundment_553a, othersurfaceimpoundment_553b, otherdisposal_554, onsitereleasetotal,"
+        + "potw_transfersforrelease_61, potw_transfersfortreatment_61, potw_totaltransfers,"
+        + "offsitereleasetotal, offsiterecycledtotal, offsiteenergyrecoverytotal, offsitetreatedtotal,"
+        + "totaltransfer, totalreleases, releases_81, energyrecoveryonsite_82, recyclingonsite_84, treatmentonsite_86,"
+        + "productionwaste_81_to_87, parentcompanyname FROM full_epa_tri_releases "
+        + "WHERE city = '" + cityName + "' AND state = '" + cityState + "') As f) As fc";
+
     // Waits for both queries to complete before sending data
     db.connect().then(obj => {
         obj.multi(query1 + query2).then(([result_geometry, result_data]) => {
@@ -75,9 +75,9 @@ app.post('/postgre-api/get-tri-releases-by-city', function(request, response) {
             });
         })
     })
-    .catch(err => {
-        return response.status(400).send(err);
-    })
+        .catch(err => {
+            return response.status(400).send(err);
+        })
 })
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
--- src/app.js
+++ src/app.js
@@ -1,231 +1,230 @@
-import React from 'react';
-import MapboxGl from 'mapbox-gl/dist/mapbox-gl.js';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js';
-import './App.css';
+import React from "react";
+import MapboxGl from "mapbox-gl/dist/mapbox-gl.js";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js";
+import "./App.css";
 
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 MapboxGl.accessToken = ACCESS_TOKEN;
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showHeader: true,
-            center: [-97.2795, 38.0282],
-            zoom: 4.20
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showHeader: true,
+      center: [-97.2795, 38.0282],
+      zoom: 4.2
     };
+  }
 
-    componentDidMount() {
-        // Instantiate a map
-        this.map = new MapboxGl.Map({
-            container: this.container,
-            style: 'mapbox://styles/mapbox/light-v9',
-            center: this.state.center,
-            zoom: this.state.zoom
-        })
+  componentDidMount() {
+    // Instantiate a map
+    this.map = new MapboxGl.Map({
+      container: this.container,
+      style: "mapbox://styles/mapbox/light-v9",
+      center: this.state.center,
+      zoom: this.state.zoom
+    });
 
-        // Instantiate geocoder
-        this.geocoder = new MapboxGeocoder({
-            accessToken: ACCESS_TOKEN,
-            countries: "US",
-            types: "place",
-            placeholder: "Enter any US city",
-            limit: 4
-        });
+    // Instantiate geocoder
+    this.geocoder = new MapboxGeocoder({
+      accessToken: ACCESS_TOKEN,
+      countries: "US",
+      types: "place",
+      placeholder: "Enter any US city",
+      limit: 4
+    });
 
-        // Geocoder result listener
-        this.geocoder.on('result', result => {
-            this.handleGeocoderResult(result);
-        });
+    this.map.on("load", () => {
+      this.map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Maki-danger-15.svg/15px-Maki-danger-15.svg.png', (error, image) => {
+        if (error) throw error;
+        this.map.addImage('danger', image);
+      });
+    })
 
-        // Add geocoder to map
-        document.getElementById('geocoder').appendChild(this.geocoder.onAdd(this.map));
+    // Geocoder result listener
+    this.geocoder.on("result", result => {
+      let pushLeftCenter = [result.result.center[0], result.result.center[1] - 1];
+      this.geocoder.setFlyTo({
+        center: pushLeftCenter,
+        zoom: 4,
+        speed: 0.5
+      });
+
+      this.handleGeocoderResult(result);
+    });
+
+    // Add geocoder to map
+    document
+      .getElementById("geocoder")
+      .appendChild(this.geocoder.onAdd(this.map));
+  }
+
+  componentDidUpdate() {
+    if (this.state.showHeader) {
+      // Place geocoder back onto the input header
+      document
+        .getElementById("geocoder")
+        .appendChild(this.geocoder.onAdd(this.map));
     }
+  }
 
-    componentDidUpdate() {
-        if (this.state.showHeader) {
-            // Place geocoder back onto the input header
-            document.getElementById('geocoder').appendChild(this.geocoder.onAdd(this.map));
-        }
-    }
+  handleGeocoderResult(result) {
+    console.log(result);
+    let pushLeftCenter = [result.result.center[0] + 0.5, result.result.center[1]];
+    this.map.flyTo({
+      center: pushLeftCenter,
+      zoom: 10,
+      speed: 0.7
+    });
 
-    handleGeocoderResult(result) {
-        let city, state;
-        let eventData = result.result;
+    let city, state;
+    let eventData = result.result;
 
-        // Parse city name
-        if (eventData.matching_text) city = eventData.matching_text.toUpperCase();
-        else city = eventData.text.toUpperCase();
+    // Parse city name
+    if (eventData.matching_text) city = eventData.matching_text.toUpperCase();
+    else city = eventData.text.toUpperCase();
 
-        // Parse two letter state code
-        let locationContext = eventData.context;
-        locationContext.map(context => {
-            if (context.id.split('.')[0] == 'region') {
-                state = context.short_code.split('-')[1];
-            }
-        })
+    // Parse two letter state code
+    let locationContext = eventData.context;
+    locationContext.map(context => {
+      if (context.id.split(".")[0] == "region") {
+        state = context.short_code.split("-")[1];
+      }
+    });
 
-        // JSON object that sends to api
-        let data = JSON.stringify({
-            city: city,
-            state: state
+    // JSON object that sends to api
+    let data = JSON.stringify({
+      city: city,
+      state: state
+    });
+
+    // Query request with city/state user input
+    let request = new Request(
+      "http://localhost:3000/postgre-api/get-tri-releases-by-city",
+      {
+        method: "POST",
+        headers: new Headers({ "Content-Type": "application/json" }),
+        body: data
+      }
+    );
+
+    // Fetch request and handle result
+    fetch(request)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.map.addSource("facilities", {
+          type: "geojson",
+          data: data.result_geometry
         });
 
-        // Query request with city/state user input
-        let request = new Request('http://localhost:3000/postgre-api/get-tri-releases-by-city', {
-            method: 'POST',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            body: data
+        this.map.addLayer({
+          id: "facilities-point",
+          type: "symbol",
+          source: "facilities",
+          layout: {
+            'icon-image': 'danger',
+            'icon-allow-overlap': true
+          }
         });
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
-        // Fetch request and handle result
-        fetch(request)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            this.map.addSource('facilities', {
-                    "type": "geojson",
-                    "data": data.result_geometry
-            });
-                 
-            this.map.addLayer({
-                "id": "facilities-heatmap",
-                "type": "heatmap",
-                "source": "facilities",
-                "maxzoom": 9,
-                "paint": {
-                    "heatmap-weight": ["interpolate", ["linear"], ["get", "mag"], 0, 0, 6, 1],
-                    "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
-                    "heatmap-color": ["interpolate", ["linear"], ["heatmap-density"], 0, "rgba(33,102,172,0)", 0.2, "rgb(103,169,207)", 0.4,
-                                      "rgb(209,229,240)", 0.6, "rgb(253,219,199)", 0.8, "rgb(239,138,98)", 1, "rgb(178,24,43)"],
-                    "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
-                    "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0]
-                }
-            }, 'waterway-label');
+    // Toggle off the input header
+    this.setState({ showHeader: false });
+  }
 
-            this.map.addLayer({
-                "id": "facilities-point",
-                "type": "circle",
-                "source": "facilities",
-                "minzoom": 7,
-                "paint": {
-                // Size circle radius by earthquake magnitude and zoom level
-                "circle-radius": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                7, [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, 1,
-                6, 4
-                ],
-                16, [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, 5,
-                6, 50
-                ]
-                ],
-                // Color circle by earthquake magnitude
-                "circle-color": [
-                "interpolate",
-                ["linear"],
-                ["get", "mag"],
-                1, "rgba(33,102,172,0)",
-                2, "rgb(103,169,207)",
-                3, "rgb(209,229,240)",
-                4, "rgb(253,219,199)",
-                5, "rgb(239,138,98)",
-                6, "rgb(178,24,43)"
-                ],
-                "circle-stroke-color": "white",
-                "circle-stroke-width": 1,
-                // Transition from heatmap to circle layer by zoom level
-                "circle-opacity": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                7, 0,
-                8, 1
-                ]
-                }
-                }, 'waterway-label');
-        })
-        .catch(err => {
-            console.log(err);
-        })
+  // Executes each time user returns to input header
+  onClick() {
+    // Reset state
+    this.setState({
+      showHeader: true,
+      center: [-97.2795, 38.0282],
+      zoom: 4.2
+    });
 
-        // Toggle off the input header
-        this.setState({ showHeader: false });
-    }
+    // Change view back to original
+    this.map.flyTo({
+      center: this.state.center,
+      zoom: this.state.zoom
+    });
 
-    // Executes each time user returns to input header
-    onClick() {
-        // Reset state
-        this.setState({
-            showHeader: true,
-            center: [-97.2795, 38.0282],
-            zoom: 4.20
-        });
+    // Reconfigure map properties
+    this.map.setCenter(this.state.center);
+    this.map.setZoom(this.state.zoom);
 
-        // Change view back to original
-        this.map.flyTo({
-            center: this.state.center,
-            zoom: this.state.zoom
-        })
+    // Clear input
+    this.geocoder.clear();
 
-        // Reconfigure map properties
-        this.map.setCenter(this.state.center);
-        this.map.setZoom(this.state.zoom);
+    // Clear map layers and data source
+    this.map.removeLayer("facilities-point");
+    this.map.removeLayer("facilities-heatmap");
+    this.map.removeSource("facilities");
+  }
 
-        // Clear input
-        this.geocoder.clear();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <div className="Map" ref={(x) => { this.container = x }}>
-                    <a href="https://github.com/nsavas/us-city-tri-visualizer" target="_blank" className="github-ribbon">
-                        <img src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></img>
-                    </a>
-                </div>
-                {this.state.showHeader ? 
-                <div className="header">
-                    <div className="header-bg">
-                        <h1>US Chemical Release Visualizer</h1>
-                        <div className="header-inner-bg">
-                        <h2>
-                            <strong>Note:</strong> This data is queried from the <a href="https://www.epa.gov/toxics-release-inventory-tri-program/tri-basic-data-files-calendar-years-1987-2017">EPA Toxic Release Inventory (TRI)</a>.
-                            TRI tracks the management of certain toxic chemicals that may pose a threat to human health and the environment.
-                            More information may be found on the website.
-                        </h2>
-                        <div id="geocoder" className="geocoder"></div>
-                        </div>
-                        <div className="header-description">
-                            <h2>
-                            Welcome to the <strong>US Chemical Release Visualizer</strong>, a tool for
-                            visualizing 30 years worth (1987-2017) of chemical release data within any US city.
-                            <br /><br />
-                            To get started, enter a US city. This will
-                            grab the data for the given city and begin the visualization.
-                            This data includes chemical name, amount of release, date of release, and much more!
-                            </h2>
-                        </div>
-                    </div>
-                </div> 
-                :
-                <div className="back-button">
-                    <button onClick={this.onClick.bind(this)}>Go Back</button>
-                </div>}
+  render() {
+    return (
+      <div className="App">
+        <div
+          className="Map"
+          ref={x => {
+            this.container = x;
+          }}
+        >
+          <a
+            href="https://github.com/nsavas/us-city-tri-visualizer"
+            target="_blank"
+            className="github-ribbon"
+          >
+            <img
+              src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+              className="attachment-full size-full"
+              alt="Fork me on GitHub"
+              data-recalc-dims="1"
+            ></img>
+          </a>
+        </div>
+        {this.state.showHeader ? (
+          <div className="header">
+            <div className="header-bg">
+              <h1>US Chemical Release Visualizer</h1>
+              <div className="header-inner-bg">
+                <h2>
+                  <strong>Note:</strong> This data is queried from the{" "}
+                  <a href="https://www.epa.gov/toxics-release-inventory-tri-program/tri-basic-data-files-calendar-years-1987-2017">
+                    EPA Toxic Release Inventory (TRI)
+                  </a>
+                  . TRI tracks the management of certain toxic chemicals that
+                  may pose a threat to human health and the environment. More
+                  information may be found on the website.
+                </h2>
+                <div id="geocoder" className="geocoder"></div>
+              </div>
+              <div className="header-description">
+                <h2>
+                  Welcome to the <strong>US Chemical Release Visualizer</strong>
+                  , a tool for visualizing 30 years worth (1987-2017) of
+                  chemical release data within any US city.
+                  <br />
+                  <br />
+                  To get started, enter a US city. This will grab the data for
+                  the given city and begin the visualization. This data includes
+                  chemical name, amount of release, date of release, and much
+                  more!
+                </h2>
+              </div>
             </div>
-        );
-    }
-
+          </div>
+        ) : (
+            <div className="back-button">
+              <button onClick={this.onClick.bind(this)}>Go Back</button>
+            </div>
+          )}
+      </div>
+    );
+  }
 }
 
 export default App;
